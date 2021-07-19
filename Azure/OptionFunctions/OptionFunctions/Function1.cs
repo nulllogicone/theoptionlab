31499d99fe720b7fdeafbf0987207cee0d2da97c
Frederic Luchting <frederic@luchting.de>
2021-07-20 00:02:50 +0200

    Show MachineName in response

--- Azure/OptionFunctions/OptionFunctions/Function1.cs
+++ Azure/OptionFunctions/OptionFunctions/Function1.cs
@@ -27,7 +27,7 @@ namespace OptionFunctions
 
             string responseMessage = string.IsNullOrEmpty(name)
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+                : $"Hello, {name}. HTTP triggered successfully on {Environment.MachineName}";
 
             return new OkObjectResult(responseMessage);
         }
